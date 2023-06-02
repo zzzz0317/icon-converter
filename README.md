22aaaa63fb9b06393df5d417846d33bfa502e800
zzzz0317 <zhangzhe0177@gmail.com>
2023-06-02 21:26:57 +0800

    English and Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# IconConverter
+
+## Usage
+
+### In cmd
+`./IconConverter.exe [source_image_path]`
+
+### Open executable file directly
+```shell
+./IconConverter.exe
+Source path:
+D:/Dev/IconConverter/IconConverter.png
+New file save to: D:/Dev/IconConverter\IconConverter_new.ico
+Press enter to exit
+```
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ try:
         flag_argv_input = True
         source_file = sys.argv[2]
     else:
-        source_file = input("输入原始图标路径:\n")
+        source_file = input("Source path:\n")
     source_path = os.path.dirname(source_file)
     source_file_name = os.path.splitext(os.path.split(source_file)[1])[0]
     new_file = os.path.join(source_path, f"{source_file_name}_new.ico")
@@ -21,10 +21,10 @@ try:
     icon_sizes = [(size, size) for size in sizes]
     img.save(new_file, sizes=icon_sizes)
 
-    print("文件保存至:", new_file)
+    print("New file save to:", new_file)
     if not flag_argv_input:
-        input("按回车退出")
+        input("Press enter to exit")
 except Exception as e:
-    print(f"发生错误({e.__class__.__name__}): {e}")
+    print(f"Error ({e.__class__.__name__}): {e}")
     print(traceback.format_exc())
-    input("按回车退出")
+    input("Press enter to exit")
